aeaffa77538cfc7d772e7b62ab3f20d422c62b36
Ashutosh <57531973+ashutosh2205x@users.noreply.github.com>
2020-06-25 16:11:53 +0530

    Add files via upload

---
+++ server/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGO_URI:
+    "mongodb+srv://user_test:LF21MqQ6KXtDprbp@cluster0-8yo26.mongodb.net/<dbname>?retryWrites=true&w=majority",
+  JWT_TOKEN: "kgWcc49Zt?Pp2+J%eh+=",
+};
